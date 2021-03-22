adf744e47b87a1e314574ee8b7095176c079d3af
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-03-22 17:31:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,41 +5,6 @@ resource "aws_key_pair" "key" {
   public_key = var.public_key
 }
 
-# Create ec2 instances
-resource "aws_instance" "host" {
-  count           =    3
-	ami             =    var.ec2_ami
-	key_name        =    var.key_name
-	subnet_id       =    var.subnet_id  
-  security_groups =    var.security_group
-  instance_type   =    var.ec2_instance_type
-  user_data       =    "${file("user-data.sh")}"
-  associate_public_ip_address = var.associate_public_ip_address
-	tags = {
-        Name = "webserver-${count.index}"  
-  }
-}
-
-# Create a Target Group 
-resource "aws_lb_target_group" "tg" {
-  name      = var.tg_name
-  port      = var.tg_port
-  protocol  = var.tg_protocol
-  vpc_id    = var.vpc_id
-
-  health_check {
-    port     = var.tg_healthcheck_port
-    protocol = var.tg_healthcheck_protocol
-  }
-}
-
-# Attach the ec2 instances to the target group
-resource "aws_alb_target_group_attachment" "instances" {
-  count = length(aws_instance.host)
-  target_group_arn = aws_lb_target_group.tg.arn
-  target_id = aws_instance.host[count.index].id
-}
-
 # Create a Network Load Balancer
 resource "aws_lb" "lb" {
   name               = var.nlb_name
@@ -50,19 +15,5 @@ resource "aws_lb" "lb" {
   tags = {
     Named = "var.nlb_tag_name"
   }
-
-  depends_on = ["aws_lb_target_group.tg"]
 }
 
-# Create a listener for NLB
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.lb.id
-  port              = "80"
-  protocol          = "TCP"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.tg.id
-    type             = "forward"
-  }
-  depends_on = ["aws_lb.lb"]
-}
