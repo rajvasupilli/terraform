2acf08c4288321ae6c5ad6aa582ef7d87486dbb8
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-03-31 13:23:47 +0530

    Added the Security Credentials

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  #access_key = "AKIAR4IWHVOBSY2RRQUR"
-  #secret_key = "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"
+  access_key = "AKIA4SYP25UZUP6RNKNO"
+  secret_key = "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"
 }
