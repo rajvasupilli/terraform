08a2ca4cce589979fe352b275fa2b60df3c3094c
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-04-01 16:59:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ terraform {
 # Create a key pair
 
 module "key_pair"  {
-  source     = "git@github.com:rajvasupilli/terraform-modules.git"
+  source     = "https://github.com/rajvasupilli/terraform-modules.git"
   key_name   = var.key_name
   public_key = var.public_key
 }
