918c8cc6aca6daa02383bf815813ab6f48aec218
rajvasupilli <30533295+rajvasupilli@users.noreply.github.com>
2021-04-04 16:28:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAUCAUK76AUVCWY277"
-  secret_key = "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"
+  access_key = "AKIA3PD72E2TTCSFHIVY"
+  secret_key = "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"
 }
