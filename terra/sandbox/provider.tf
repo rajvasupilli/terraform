a1481cef30cdc605b9f7e20fa650a15489124d82
EC2 Default User <ec2-user@ip-172-31-19-110.us-west-1.compute.internal>
2021-04-13 09:41:04 +0000

    Added Sec Creds

--- terra/sandbox/provider.tf
+++ terra/sandbox/provider.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA3PD72E2TTCSFHIVY"
-  secret_key = "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"
+  access_key = "AKIAYAFLFRDBMFKFWVEH"
+  secret_key = "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"
 }
